0446c918cdddc445141b06f45111cb2e4791bdc6
Babajide Williams <babanila@yahoo.com>
2026-05-17 11:40:15 +0200

    chore: update eslint config

--- tooling/eslint-config/base.cjs
+++ tooling/eslint-config/base.cjs
@@ -1,12 +1,19 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks'],
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'react',
+    'react-hooks',
+  ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'prettier',
   ],
   settings: {
@@ -25,6 +32,15 @@ module.exports = {
       'error',
       {
         argsIgnorePattern: '^_',
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
       },
     ],
   },
