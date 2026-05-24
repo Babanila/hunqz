12590951ff45df62e86b2e55e40df4a6c7ffd200
Babajide Williams <babanila@yahoo.com>
2026-05-25 01:48:12 +0200

    feat: Add NextImage component

---
+++ apps/web/src/components/next-image.tsx
@@ -0,0 +1,26 @@
+import type { SharedImageProps } from '@repo/ui';
+import Image from 'next/image';
+
+export function NextImage({
+  src,
+  alt,
+  className,
+  loading = 'lazy',
+  width,
+  height,
+  priority = false,
+  sizes,
+}: SharedImageProps) {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      className={className}
+      width={width}
+      height={height}
+      priority={priority}
+      loading={priority ? undefined : loading}
+      sizes={sizes}
+    />
+  );
+}
