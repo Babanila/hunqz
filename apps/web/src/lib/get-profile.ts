f43edf9e0baf81a2ab021752e745ebb25338500f
Babajide Williams <babanila@yahoo.com>
2026-05-25 01:50:29 +0200

    feat: Add get-profile lib

---
+++ apps/web/src/lib/get-profile.ts
@@ -0,0 +1,36 @@
+import { fetcher, FetchError } from '@repo/shared/fetcher';
+import { ProfileData } from '@repo/ui/index';
+
+import { apiUrlClient } from './api-client';
+
+export async function getProfile(id: string) {
+  try {
+    const profile = await fetcher<ProfileData>(apiUrlClient.getApiUrl(id), {
+      next: { revalidate: 300, tags: ['hunqz-profiles'] },
+    });
+    return { data: profile, error: null };
+  } catch (error) {
+    if (error instanceof FetchError) {
+      console.error(`[${error.status}] ${error.url}:`, error.body);
+
+      return {
+        data: null,
+        error: {
+          message: error.message,
+          status: error.status,
+          statusText: error.statusText,
+          body: error.body,
+        },
+      };
+    }
+
+    return {
+      data: null,
+      error: {
+        message: error instanceof Error ? error.message : 'Unknown error',
+        status: 500,
+        statusText: 'Internal Server Error',
+      },
+    };
+  }
+}
