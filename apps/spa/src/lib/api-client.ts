e9af75fd0436791b611947642b5c6269bd96a454
Babajide Williams <babanila@yahoo.com>
2026-05-25 01:49:56 +0200

    feat: Add api-client using createUrlBuilder

---
+++ apps/spa/src/lib/api-client.ts
@@ -0,0 +1,9 @@
+import { createUrlBuilder } from '@repo/shared';
+
+import { env } from '../env';
+
+export const apiUrlClient = createUrlBuilder({
+  baseUrl: env.VITE_BASE_DOMAIN,
+  apiPath: env.VITE_API_BASE_PATH,
+  imagePath: env.VITE_API_IMAGE_PATH,
+});
