27fcd2fb273e1b8a0dbf056f59c8d88164663eae
Babajide Williams <babanila@yahoo.com>
2026-05-25 02:54:43 +0200

    feat: Update spa app

---
+++ apps/spa/src/env.ts
@@ -0,0 +1,5 @@
+export const env = {
+  VITE_BASE_DOMAIN: import.meta.env.VITE_BASE_DOMAIN,
+  VITE_API_BASE_PATH: import.meta.env.VITE_API_BASE_PATH,
+  VITE_API_IMAGE_PATH: import.meta.env.VITE_API_IMAGE_PATH,
+};
